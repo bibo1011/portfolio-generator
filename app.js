56073a8bc22de6d28a2f2f79a2944e9c56b6b250
Blue Abad <bibo1011@gmail.com>
2020-09-16 16:54:44 -0700

    install inquirer npm

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
+
+
+
+
+// Module Sample After This Line
+
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
 // const printProfileData = profileDataArr => {
@@ -24,11 +51,7 @@ const [name, github] = profileDataArgs;
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
 
 
 
