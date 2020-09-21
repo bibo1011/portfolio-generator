47e3fac9e299c76a1214041b9d602d49c36cac74
Blue Abad <bibo1011@gmail.com>
2020-09-20 22:08:21 -0700

    // fs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 
@@ -135,11 +135,11 @@ promptUser()
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
